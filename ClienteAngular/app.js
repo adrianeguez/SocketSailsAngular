37dd772b288b69b2527de14ba0a66712b9ebb6c1
Adrian Eguez <eadepto@hotmail.com>
2016-01-26 12:55:30 -0500

    Arreglo de Codigo Nombre de variables y Ejercicio

--- ClienteAngular/app.js
+++ ClienteAngular/app.js
@@ -1,89 +1,118 @@
+//Definimos la URL de Sails para trabajar cuando no este dentro del servidor Sails la aplicacion de Angular
 io.sails.url = "http://localhost:1337";
 
+//Creamos una nueva aplicacion llamada Chat
 var app = angular.module('chat', []);
 
+//Definimos el Controlador 'main' con las dependencias $scope y $http
 app.controller('main', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.variable;
-  
-  io.socket.get('http://localhost:1337/Chat/conversacion');
-  
+  //Constructor
+  //Variable nuevo chat para enviar un nuevo chat con websocket o con http
+  $scope.nuevoChatHttp;
+  $scope.nuevoChatWebSocket;
+
+  //Nos suscribimos gracias al 'Blueprint' de Sailsjs con el modelo Chat
+  //Ademas de esto Sails nos trae todos los datos del servidor
+  //Aqui populamos la variable 'chatsWs'
+  io.socket.get('http://localhost:1337/Chat',
+    function (resData, jwres) {
+      console.log('Se suscribio con blueprint de Sailsjs')
+      console.log(resData);
+      $scope.chatsWs = resData;
+      //$digest() es necesario para que se actualice en la vista
+      $scope.$digest();
+    });
+
+  //Nos suscribimos con el metodo suscribirseOPublicar dentro del controlador de Chat en Sailsjs que nosotros creamos
+  io.socket.get('http://localhost:1337/Chat/suscribirseOPublicar',
+    function (resData, jwres) {
+      console.log('Se suscribio con nuestro metodo suscribirseOPublicar...');
+      console.log(jwres);
+      console.log('No hay datos porq es nuestro metodo...');
+      console.log(resData);
+    });
+
+  //Llamamos a los Chats con HTTP
+  //Aqui populamos la variable 'chatsHttp'
   $http({
     method: 'GET',
     url: 'http://localhost:1337/Chat'
   }).then(
     function (resp) {
+      console.log('Chats traidos con un GET HTTTP')
       console.log(resp);
-      $scope.chat = resp.data;
+      $scope.chatsHttp = resp.data;
 
     },
     function (err) {
       console.log(err);
-
     });
-  
-  
-  io.socket.on('chat',function(objQueLLegaCuandoCreaUnChat){
-    console.log('Escupio chat el servidor');
+  //Creamos un nuevo chat mediante HTTP
+  $scope.llamarConHttpPost = function () {
+
+    console.log('Entro a llamada con HTTp');
+
+    $http({
+      method: 'POST',
+      url: 'http://localhost:1337/Chat',
+      data: {
+        nombre: $scope.nuevoChatHttp
+      }
+    }).then(
+      function (resp) {
+        console.log(resp);
+
+      },
+      function (err) {
+        console.log(err);
+
+      });
+
+    $scope.nuevoChatHttp = "";
+
+  }
+  //Creamos un nuevo chat mediante WebSockets
+  $scope.llamarConSocketPost = function () {
+    console.log('Entro a llamada con WebSocket');
+    io.socket.post(
+      'http://localhost:1337/Chat/suscribirseOPublicar', {
+        nombre: $scope.nuevoChatWebSocket
+      });
+    $scope.nuevoChatWebSocket = "";
+
+
+  }
+
+  //Escuchamos que el servidor nos responda 'Chat' (nombre de modelo en sails) 
+  //para saber si se actualizo, borro, creo o modifico un registro
+  io.socket.on('chat', function (objQueLLegaCuandoCreaUnChat) {
+    console.log('Respondio del Servidor');
     console.log(objQueLLegaCuandoCreaUnChat);
+    console.log('Verbo');
+    console.log(objQueLLegaCuandoCreaUnChat.verb);
     
-    if(objQueLLegaCuandoCreaUnChat.verb === 'created'){
-      $scope.chat.push(objQueLLegaCuandoCreaUnChat.data);
+    //Aqui esta implementado solamente cuando el servidor nos responde que creamos un registro
+    if (objQueLLegaCuandoCreaUnChat.verb === 'created') {
+      //Agregamos tanto al arreglo 'chatsHttp' como al arreglo 'chatsWs'
+      $scope.chatsHttp.push(objQueLLegaCuandoCreaUnChat.data);
+      $scope.chatsWs.push(objQueLLegaCuandoCreaUnChat.data);
+      $scope.$digest();
+    }
+    if (objQueLLegaCuandoCreaUnChat.verb === 'destroyed') {
+      //Escribir el codigo para cuando se borre un registro, buscarlo en nuestro arreglo local
+      //y eliminarlo del arreglo
+      $scope.$digest();
+    }
+    if (objQueLLegaCuandoCreaUnChat.verb === 'updated') {
+      //Escribir el codigo para cuando se actualice un registro, buscarlo en nuestro arreglo local
+      //y actualizarlo
       $scope.$digest();
     }
     
+    
+    
   })
 
-  //  io.socket.get('http://localhost:1337/Chat/conversacion');
-
-
-  //  io.socket.request({
-  //    method: 'get',
-  //    url: 'http://localhost:1337/Chat/suscribirse'
-  //  })
-
-  //  $http({
-  //    method: 'GET',
-  //    url: 'http://localhost:1337/Chat/suscribirse'
-  //  }).then(
-  //    function (resp) {
-  //      console.log(resp);
-  //
-  //    },
-  //    function (err) {
-  //      console.log(err);
-  //
-  //    });
-
-
-
-
-
-  $scope.llamar = function () {
-    console.log('penes');
-    io.socket.post(
-      'http://localhost:1337/Chat/conversacion', {
-        nombre: $scope.variable
-      });
-    $scope.variable = "";
-
-    //    
-    //    $http({
-    //      method: 'POST',
-    //      url: 'http://localhost:1337/Chat/conversacion',
-    //      data: {
-    //        nombre: $scope.variable
-    //      }
-    //    }).then(
-    //      function (resp) {
-    //        console.log(resp);
-    //
-    //      },
-    //      function (err) {
-    //        console.log(err);
-    //
-    //      });
-
-  }
 
 }]);
--- SocketSails/api/controllers/ChatController.js
+++ SocketSails/api/controllers/ChatController.js
@@ -6,55 +6,49 @@
  */
 
 module.exports = {
-  suscribirse:function(req, res){
+    suscribirseOPublicar: function (req, res) {
 
-    console.log(req.socket);
+        console.log('Entro a la función del Controlador Chat');
+        //guardamos los parámetros en una variable
+        var parametros = req.params.all();
+        //Vamos a hacer una comparación, si es que la petición es con webssockets y es un método post creamos y publicamos el Chat
+        if (req.isSocket && req.method === 'POST') {
+          console.log('Es un websocket y usa un método POST', parametros.nombre);
 
 
-  },
-  conversacion: function (req, res) {
+          //Aqui estamos registrando el chat en la Base de Datos
+          Chat.create({
+            nombre: parametros.nombre
+          }).exec(function (err, nuevoChatCreado) {
+            //Si hay error imprimimos el error
+            if (err) console.log(err);
+            console.log('se creo el mensaje del chat: ', nuevoChatCreado);
 
-    console.log('entro arrechisimo');
-
-    var parametros = req.params.all();
-    if (req.isSocket && req.method === 'POST') {
-      console.log('valens verga bien', parametros.nombre);
-      Chat.create(
-        {
-          nombre:parametros.nombre
-        }).exec(function(err,nuevoChatCreado){
-          console.log('se creo el mensaje del chat: ',nuevoChatCreado);
-          Chat.publishCreate(
-            {
-              nombre:nuevoChatCreado.nombre,
-              id:nuevoChatCreado.id,
-              quemado: new Date()
+            //Si no hay error publicamos el chat por medio de los websockets y podemos enviar nuevos parametros como el "fechaDelServidor"
+            Chat.publishCreate({
+              nombre: nuevoChatCreado.nombre,
+              id: nuevoChatCreado.id,
+              fechaDelServido: new Date()
             });
-        Chat.publishUpdate(
-          {
-            id:nuevoChatCreado.id
-          },
-          {
-            nombre:nuevoChatCreado.nombre,
-            quemado: new Date()
+
+
           });
-        Chat.publishDestroy(
-          {
-            id:nuevoChatCreado.id
-          });
+          //Aqui definimos si la peticion es con websocket entonces lo suscribimos
+        } else if (req.isSocket) {
 
+          //Suscribimos al usuario a Chat
+          Chat.watch(req.socket);
+          console.log('Se ha suscrito con el id de socket: ' + req.socket.id);
+          return res.ok('Suscrito con el id: '+req.socket.id);
 
-      });
+        } else {
 
-      //return res.ok();
-    }
-    else if (req.isSocket){
+          //Si no se suscribe ni se crea un chat, entonces desplegar en consola que es un bad request.
+          console.log('Bad Request')
 
-      Chat.watch(req.socket);
-
-      console.log('valen verga mal' + req.socket.id);
 
     }
+
 
 
 
